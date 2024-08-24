9d9a595109b03b49779aa8b5f09b66eaaf2ceeef
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-08-24 11:07:39 +0500

    update Mod7/Work7-8

--- Mod7/Work7-8.cpp
+++ Mod7/Work7-8.cpp
@@ -9,13 +9,18 @@ int main() {
     std::cout << "Введите высоту ёлочки: ";
     std::cin >> height;
 
-    for (int i = 0; i < height; i++) {
-        for (int j = i + 1; j < height; j++)
-            std::cout << " ";
-        for (int j = 0; j <= 2 * i; j++)
-            std::cout << "#";
-        if (i < height - 1) {
-            std::cout << "\n";
+    if (height < 0){
+        std::cout << "Вы ввели неверное значение!\n";
+        std::cout << "Введенное значение должно быть положительным!\n";
+    } else {
+        for (int i = 0; i < height; i++) {
+            for (int j = i + 1; j < height; j++)
+                std::cout << " ";
+            for (int j = 0; j <= 2 * i; j++)
+                std::cout << "#";
+            if (i < height - 1) {
+                std::cout << "\n";
+            }
         }
     }
     return 0;
