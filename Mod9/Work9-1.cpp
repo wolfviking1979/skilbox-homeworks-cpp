0c80e7ece657ee8782e1ee8a240ed0d435cf90e5
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-08-29 15:52:22 +0500

    update Mod9/Work9-1

--- Mod9/Work9-1.cpp
+++ Mod9/Work9-1.cpp
@@ -1,50 +1,70 @@
 
 
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Время в пути\n";
-    std::cout << "---------------------------------------------------\n";
-    std::cout << "Введите время отправления (HH:MM): ";
-    std::string departureTime;
-    std::cin >> departureTime;
-    std::cout << "Введите время прибытия (HH:MM): ";
-    std::string arrivalTime;
-    std::cin >> arrivalTime;
-    // проверка на корректность ввода данных: HH:MM
-    if (departureTime.length() != 5 || arrivalTime.length() != 5 || departureTime[2] != ':' ||
-        arrivalTime[2] != ':' || departureTime[0] < '0' || departureTime[0] > '2' ||
-        departureTime[1] < '0' || departureTime[1] > '9' || departureTime[3] < '0' ||
-        departureTime[3] > '5' || departureTime[4] < '0' || departureTime[4] > '9' ||
-        arrivalTime[0] < '0' || arrivalTime[0] > '2' || arrivalTime[1] < '0' ||
-        arrivalTime[1] > '9' || arrivalTime[3] < '0' || arrivalTime[3] > '5' ||
-        arrivalTime[4] < '0' || arrivalTime[4] > '9') {
-        std::cout << "Ошибка ввода!\n";
-    } else {
-        // Конвертация строк в число
-        int departureHours = 10 * (departureTime[0] - '0') + (departureTime[1] - '0');
-        int departureMinutes = 10 * (departureTime[3] - '0') + (departureTime[4] - '0');
-        int arrivalHours = 10 * (arrivalTime[0] - '0') + (arrivalTime[1] - '0');
-        int arrivalMinutes = 10 * (arrivalTime[3] - '0') + (arrivalTime[4] - '0');
-        // проверка на превышение времени
-        if (departureHours > 23 || departureMinutes > 59 || arrivalHours > 23 || arrivalMinutes > 59) {
-            std::cout << "Ошибка ввода!\n";
-        } else {
-            int travelHours;
-            int travelMinutes = 0;
-            if (departureHours > arrivalHours) {
-                travelHours = 24 - departureHours + arrivalHours;
-                travelMinutes = arrivalMinutes - departureMinutes;
+    std::string departure;
+    std::string arrival;
 
-            } else {
-                travelHours = arrivalHours - departureHours;
-            }
-            if (arrivalMinutes < departureMinutes) {
-                travelHours--;
-            } else {
-                travelMinutes = arrivalMinutes - departureMinutes;
-            }
-            std::cout << "Поездка составила " << travelHours << " ч. " << travelMinutes << " мин.\n";
+    int departureHours;
+    int departureMinutes;
+
+    int arrivalHours;
+    int arrivalMinutes;
+
+    while (true) {
+        std::cout << "Введите время отправления(hh:mm):\n";
+        std::cin >> departure;
+        std::cout << "Введите время прибытия(hh:mm):\n";
+        std::cin >> arrival;
+
+        if (departure.length() != 5 || arrival.length() != 5) {
+            std::cout << "Ошибка ввода! Неверная длина!\n";
+            continue;
         }
+
+        if (arrival[0] < '0' || arrival[0] > '2' || arrival[1] < '0' || arrival[1] > '9'
+            || departure[0] < '0' || departure[0] > '2' || departure[1] < '0' || departure[1] > '9') {
+            std::cout << "Ошибка ввода! Неверный формат часов!\n";
+            continue;
+        }
+
+        if (arrival[3] < '0' || arrival[3] > '5' || arrival[4] < '0' || arrival[4] > '9'
+            || departure[3] < '0' || departure[3] > '5' || departure[4] < '0' || departure[4] > '9') {
+            std::cout << "Ошибка ввода! Неверный формат минут!\n";
+            continue;
+        }
+
+        if (arrival[2] != ':' || departure[2] != ':') {
+            std::cout << "Ошибка ввода! Неверный делитель!\n";
+            continue;
+        }
+
+        // Конвертация времени из строчного типа в целочисленный тип
+        departureHours = (departure[0] - '0') * 10 + (departure[1] - '0');
+        departureMinutes = (departure[3] - '0') * 10 + (departure[4] - '0');
+
+        arrivalHours = (arrival[0] - '0') * 10 + (arrival[1] - '0');
+        arrivalMinutes = (arrival[3] - '0') * 10 + (arrival[4] - '0');
+
+        if (departureHours >= 24 || departureMinutes > 59
+            || arrivalHours >= 24 || arrivalMinutes > 59) {
+            std::cout << "Ошибка ввода! Неверный формат времени\n";
+            continue;
+        }
+        break;
     }
+
+    // Вычисление времени в пути
+    int departureTime = departureHours * 60 + departureMinutes;
+    int arrivalTime = arrivalHours * 60 + arrivalMinutes;
+
+    int travelTime;
+    if (departureTime >= arrivalTime)
+        travelTime = 24 * 60 - departureTime + arrivalTime;
+    else
+        travelTime = arrivalTime - departureTime;
+
+    std::cout << "Время в пути: " << travelTime / 60 << " часов, " << travelTime % 60 << " минут"<< std::endl;
 }
